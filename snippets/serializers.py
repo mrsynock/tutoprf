659114140d1ebee9e611a6bcd2318219160bcc55
Jimmy Girardet <ijkl@tuta.io>
2017-07-12 22:41:27 +0200

    prelancement serveur windows

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,16 +1,8 @@
-from rest_framework import serializers
+from rest_framework import ModelSerializer
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    lineos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def creat(self, validated_data):
-        """
-        Create and retrun a nex 'snippet', selon la validation
-        """
-        return Snippet.objects.create(**validated_data)
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
